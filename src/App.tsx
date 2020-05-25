30cca45db6158b1886af5c2273716f769cf93bbf
Peter Lobo <peter.lobo@mjv.com.br>
2020-05-25 13:01:15 -0500

    update app to fucntional

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@ import logo from './logo.svg';
 import {Card} from './components/card/card.component';
 import './App.css';
 import {LoginPage} from "./pages/login/login";
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Redirect, useHistory} from 'react-router-dom';
 
 
-function App() {
-  return (
-      <>
+export const App = (props: any) => {
+    const history = useHistory();
+  return <>
       <BrowserRouter >
             <Switch>
                 <Route exact path="/game" component={Card}/>
                 <Route exact path='/login' component={LoginPage}/>
             </Switch>
         </BrowserRouter>
+          <button onClick={() => window.location.href='/login'}>Login</button>
       </>
-  );
 }
 
-export default App;
