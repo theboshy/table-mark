4b1326486eef16355ed043bddc912d5910538516
Peter Lobo <peter.lobo@mjv.com.br>
2020-05-25 13:29:10 -0500

    update pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import logo from './logo.svg';
 import {Card} from './components/card/card.component';
 import './App.css';
 import {LoginPage} from "./pages/login/login";
-import {BrowserRouter, Route, Switch, Redirect, useHistory} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Redirect, useHistory, HashRouter} from 'react-router-dom';
 
 export const App = (props: any) => {
     const history = useHistory();
   return <>
-      <BrowserRouter >
+    <BrowserRouter >
             <Switch>
                 <Route exact path={'./game'} component={Card}/>
                 <Route exact path={'./login'} component={LoginPage}/>
             </Switch>
         </BrowserRouter>
-      <h1>{process.env.PUBLIC_URL}</h1>
-          <button onClick={() => window.location.href=process.env.PUBLIC_URL + '/login'}>Login</button>
+          <button onClick={() =>  './login'}>Login</button>
       </>
 }
 
