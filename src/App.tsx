d862abbc1aeee9295b1ef393b97dc9ce0e3d33de
Peter Lobo <peter.lobo@mjv.com.br>
2020-05-25 11:58:18 -0500

    update swicth

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import {Card} from './components/card/card.component';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import {LoginPage} from "./pages/login/login";
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   return (
       <>
-      <Router>
-        <Route path="/game" exact component={Card}/>
-      </Router>
-      <LoginPage ></LoginPage>
+      <BrowserRouter >
+        <Switch>
+            <Route exact path="/game" component={Card}/>
+                <Route exact path='/login' component={LoginPage}/>
+            </Switch>
+        </BrowserRouter>
       </>
   );
 }
