9b6429713fce8f1d7c84a3007adad9714de1b06f
Peter Lobo <peter.lobo@mjv.com.br>
2020-05-25 13:17:27 -0500

    update ppublic url

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import './App.css';
 import {LoginPage} from "./pages/login/login";
 import {BrowserRouter, Route, Switch, Redirect, useHistory} from 'react-router-dom';
 
-
 export const App = (props: any) => {
     const history = useHistory();
   return <>
       <BrowserRouter >
             <Switch>
-                <Route exact path={process.env.PUBLIC_URL + '/game'} component={Card}/>
-                <Route exact path={process.env.PUBLIC_URL + '/login'} component={LoginPage}/>
+                <Route exact path={'./game'} component={Card}/>
+                <Route exact path={'./login'} component={LoginPage}/>
             </Switch>
         </BrowserRouter>
+      <h1>{process.env.PUBLIC_URL}</h1>
           <button onClick={() => window.location.href=process.env.PUBLIC_URL + '/login'}>Login</button>
       </>
 }
