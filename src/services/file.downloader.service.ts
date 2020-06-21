7292fa956eda47f615385e9a6e2fafacec79307d
theboshy <akumaokami102gmail.com>
2020-06-21 05:30:59 +0200

    update file downlaoder

--- src/services/file.downloader.service.ts
+++ src/services/file.downloader.service.ts
@@ -2,7 +2,7 @@ import FileSaver from 'file-saver'
 
 export class FileDownloader  {
 
- downloadFile =(fileName: string) => {
+ downloadFile = (fileName: string) => {
      FileSaver.saveAs("http://localhost:3000/resources/" + fileName, fileName);
  }
 
